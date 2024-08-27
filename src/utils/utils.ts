5412705611b245c9a4bc994c117df90dc0cc3bea
Aleem Isiaka <aleemisiaka@gmail.com>
2024-08-27 08:13:00 +0100

    prevent attempting to analyze null/undefined data

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,7 @@
 export const isString = (value: any) => typeof value === 'string';
 export const isObject = (value: any) =>
-  value && Object.prototype.toString.call(value) === '[object Object]';
-export const isArray = (value: any) => value && Array.isArray(value);
+  !!value && Object.prototype.toString.call(value) === '[object Object]';
+export const isArray = (value: any) => !!value && Array.isArray(value);
 
 const isBrowser = () => {
   try {
